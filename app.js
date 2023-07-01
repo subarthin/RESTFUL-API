2a6c39683e7859358f18fcf33f9d45c49a1fe135
subarthin raja <subarthinraja@gmail.com>
2023-07-01 18:25:27 +0530

    v1.1

--- app.js
+++ app.js
@@ -47,38 +47,25 @@ app.route("/articles")
     });
 });
 
-// app.get("/articles",function(req,res){
-//      Article.find().then(function(data){
-//         res.send(data);
-//      }).then(function(err){
-//         console.log(err);
-//      })
-// })
-
-// app.post("/articles",function(req,res){
-//     let t=req.body.title;
-//     let c=req.body.content;
-//     // console.log(t);
-//     // console.log(c);
-//     const item=new Article({
-//         title:t,
-//         content:c
-//     });
-//     item.save().then(function(){
-//         res.send("success inerted into database");
-//     }).catch(function(err){
-//         res.send(err);
-//     });
-// })
-
-// app.delete("/articles",function(req,res){
-//     Article.deleteMany().then(function(){
-//         res.send("deleted")
-//     }).catch(function(err){
-//         res.send(err);
-//     });
-// });
-
+app.route("/articles/:name")
+.get(function(req,res){
+    Article.findOne({title:req.params.name}).then(function(data){
+        res.send(data);
+    }).catch(function(err){
+        res.send("err");
+    })
+})
+.put(function(req,res){
+    Article.updateOne(
+        {title:req.params.name},
+        {title:req.body.title,
+        content:req.body.content},
+        {overwrite:false}).then(function(data){
+            res.send(data)
+        }).catch(function(err){
+            res.send("error")
+        });
+})
 
 app.listen(port,()=>{
     console.log("Server running at port 3000");
